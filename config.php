091ee50de856b8ea6042220fa1206b5211ee8cd7
Maciej Sypien <maciejsypien@gmail.com>
2014-04-14 23:32:09 +0200

    Added config.php file

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+define('MEDIA_BASEDIR','images/');
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','ropa');
+define('DB_NAME','private_zoo');
+
+/*
+ * Setting for the local time zone to prevent form warrings
+ * in case of using time and date functions
+ */
+date_default_timezone_set('Europe/Warsaw');
+
+?>
