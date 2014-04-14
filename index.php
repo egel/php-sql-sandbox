2191c01b98c8922043d840cc11fc00c8f0203ae9
Maciej Sypien <maciejsypien@gmail.com>
2014-04-14 22:26:09 +0200

    Improved index file with sql connection and exceptions handling

--- index.php
+++ index.php
@@ -7,15 +7,27 @@
  * Created by egel 2011 - Maciej Sypień
  */
 
-/*
- * Simple function to autoload classes
- */
-function __autoload_my_classes($className)
-{
-	require_once 'classes/'.$className.'.class.php';
-}
-spl_autoload_register('__autoload_my_classes');
+// Order of loading files is important!
+include_once 'config.php'; // Load simple local config
+include_once 'methods.php'; // Load simple local methods
 
-$zoo = new Zoo();
-$zoo->startTour();
+try
+{
+  /*
+   *  Test MySQL connection or show error
+   */
+  $lacz = connect_db();
+
+  // Start the project
+  $zoo = new Zoo();
+  $zoo -> startTour();
+}
+catch(Exception $e)
+{
+  echo "<p>";
+  echo "Exception occured in " . $e->getFile() . " on line " . $e->getLine() . "<br/>";
+  echo "Message: " . $e->getMessage() . "</br>";
+  echo "Code of error: " . $e->getCode();
+  echo "</p>";
+}
 ?>
